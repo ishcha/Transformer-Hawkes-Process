a38c1118cb62e08ab139eb31f36c1b81869a9218
Isha Chaudhary <51634188+ishcha@users.noreply.github.com>
2022-01-23 00:34:36 +0530

    Some comments for clarity

--- transformer/Modules.py
+++ transformer/Modules.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+# For the self-attention module
 class ScaledDotProductAttention(nn.Module):
     """ Scaled Dot-Product Attention """
 
@@ -13,12 +13,12 @@ class ScaledDotProductAttention(nn.Module):
         self.dropout = nn.Dropout(attn_dropout)
 
     def forward(self, q, k, v, mask=None):
-        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3)) # While k is (batch_size, heads, number of event points in a sequence, M_k) dimensional, transpose the last 2 dimensions
 
         if mask is not None:
-            attn = attn.masked_fill(mask, -1e9)
+            attn = attn.masked_fill(mask, -1e9) # mask with -infty values on future value correlations
 
-        attn = self.dropout(F.softmax(attn, dim=-1))
-        output = torch.matmul(attn, v)
-
+        attn = self.dropout(F.softmax(attn, dim=-1)) # softmax over the last dimension of the computed attention, with dropout of attention (randomly 0ing attn values). 
+        output = torch.matmul(attn, v) # multiply with the value matrix
+        # computation of single-head attention values done!: output
         return output, attn
